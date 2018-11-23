6ede98159815692d53d4def123e0e6bc42550e16
Gleb Danilov <glebdangl@gmail.com>
2018-11-23 09:53:10 +0800

    laba3v3

--- Laba3/Laba3/Main.cs
+++ Laba3/Laba3/Main.cs
@@ -12,6 +12,64 @@ namespace Laba3
 {
     public partial class Main : Form
     {
+        public class Global
+        {
+            public static KomplexNumber[] kmp = new KomplexNumber[0];
+            public static DrobNumber[] drb = new DrobNumber[0];
+            
+        }
+        public class Number
+        {
+            public static int count = 0;
+            public Number()
+            { count++; }
+            protected double nmbvalue;
+            public double GetValue
+            { get { return nmbvalue; } } 
+        }
+            public class KomplexNumber : Number
+            {
+                double exictedpart;
+                double fakepart;
+                public double Transfer()
+                {
+                    nmbvalue = Math.Sqrt(Math.Pow(exictedpart, 2) + Math.Pow(fakepart, 2));
+                    nmbvalue = Math.Round(nmbvalue, 4);
+                    return nmbvalue;
+                }
+                public double Fakepart
+                {
+                    get { return fakepart; }
+                    set { fakepart = value; }
+                }
+                public double Exictedpart
+                {
+                    get { return exictedpart; }
+                    set { exictedpart = value; }
+                }
+            }
+        
+        public class DrobNumber : Number
+        {
+            double numerator, denominator;
+            public double Transfer()
+            {
+                nmbvalue = numerator/denominator;
+                nmbvalue = Math.Round(nmbvalue, 4);
+                return nmbvalue;
+            }
+            public double Numerator
+            {
+                get { return numerator; }
+                set { numerator = value;}
+            }
+            public double Denominator
+            {
+                get { return denominator; }
+                set { denominator = value; }
+            }
+        }
+        
         public Main()
         {
             InitializeComponent();
@@ -48,7 +106,7 @@ namespace Laba3
         private void findToolStrip_Click(object sender, EventArgs e)
         {
             FindNmbList form = new FindNmbList();
-            form.Show
+            form.Show();
         }
     }
 }
--- Laba3/Laba3/secondForms/AddNumbForm.cs
+++ Laba3/Laba3/secondForms/AddNumbForm.cs
@@ -19,17 +19,30 @@ namespace Laba3
 
         private void addButton_Click(object sender, EventArgs e)
         {
-
+            if (drobCheckBox.Checked == true)
+            {
+                Main.DrobNumber x = new Main.DrobNumber();
+                x.Numerator = Convert.ToDouble(maskedTextBox1.Text);
+                x.Denominator = Convert.ToDouble(maskedTextBox2.Text);
+                x.Transfer();
+                MessageBox.Show("обьект создан его запись" + Main.Number.count);
+            }
+            if (komplexCheckBox.Checked == true)
+            {
+                Main.KomplexNumber x = new Main.KomplexNumber();
+                x.Exictedpart = Convert.ToDouble(maskedTextBox1.Text);
+                x.Fakepart = Convert.ToDouble(maskedTextBox2.Text);
+                MessageBox.Show("обьект создан его запись" + Main.Number.count);
+                Array.Resize(ref Main.Global.drb, Main.Global.drb.Length + 1);
+                Main.Global
+            }
         }
 
         private void drobCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            if (komplexCheckBox.Checked == true)
-            {
-                komplexCheckBox.Checked = false;
-                komplexlabel1.Visible = false;
-                komplexlabel2.Visible = false;
-            }
+            komplexCheckBox.Checked = false;
+            komplexlabel1.Visible = false;
+            komplexlabel2.Visible = false;
             droblabel1.Visible = true;
             droblabel2.Visible = true;
             maskedTextBox1.Visible = true;
@@ -39,12 +52,9 @@ namespace Laba3
 
         private void komplexCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-            if (drobCheckBox.Checked == true)
-            {
-                drobCheckBox.Checked = false;
-                droblabel1.Visible = false;
-                droblabel2.Visible = false;
-            }
+            drobCheckBox.Checked = false;
+            droblabel1.Visible = false;
+            droblabel2.Visible = false;
             komplexlabel1.Visible = true;
             komplexlabel2.Visible = true;
             maskedTextBox1.Visible = true;
